9cca0380703809c9d55f82645cca2e9c8ba94d79
Lucas Telles <lltellesss@gmail.com>
2023-10-10 17:44:18 -0300

    number/multiplicator input js working

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// seleçao de elementos
+const multiplicationForm = document.querySelector('#multiplication-form')
+const numberInput = document.querySelector('#number')
+const multiplicationInput = document.querySelector('#multiplicator')
+
+// funcoes
+
+// eventos
+multiplicationForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const multiplicationNumber = +numberInput.value
+    const multiplicatorNumber = +multiplicationInput.value
+
+    if(!multiplicationNumber || !multiplicatorNumber) return
+
+    console.log(multiplicationNumber, multiplicatorNumber)
+})
